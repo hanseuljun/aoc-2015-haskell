7c75a63e8ad526a00eb3cbe1dc4b4e97299ccb05
Hanseul Jun <hanseuljun@gmail.com>
2026-08-15 23:15:18 -0700

    day1-2.hs

---
+++ day1-2.hs
@@ -0,0 +1,7 @@
+main :: IO ()
+main = do
+    contents <- readFile "input.txt"
+    let deltas = map (\x -> if x == '(' then 1 else -1) contents
+    let floors = scanl (+) 0 deltas
+    let overgroundFloors = takeWhile (>= 0) floors
+    print (length overgroundFloors)
