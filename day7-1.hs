6a374dceb900d6fd6b1a3eb4d6c4f14511ea5768
Hanseul Jun <hanseuljun@gmail.com>
2026-08-17 23:25:42 -0700

    lookupOrRead

--- day7-1.hs
+++ day7-1.hs
@@ -3,6 +3,7 @@ import Data.List.Extra (trim)
 import Data.List.Split (splitOn)
 import qualified Data.Map as Map
 import Data.Map (Map)
+import Debug.Trace (trace)
 import Data.Word (Word16)
 
 example :: String
@@ -16,14 +17,32 @@ example :: String
 --           \NOT y -> i"
 
 example = "123 -> x\n\
-          \456 -> y"
+          \456 -> y\n\
+          \x AND y -> d"
+
+lookupOrRead :: (Map String Word16) -> String -> Word16
+lookupOrRead map word
+  | Map.member word map = map Map.! word
+  | otherwise = read word :: Word16
+
+traceWords :: [String] -> [String]
+traceWords xs = trace ("words: " ++ (show xs)) xs
+
+evaluate :: (Map String Word16) -> String -> Word16
+evaluate map expression =
+  let expressionWords = traceWords (words expression)
+  -- in read expression :: Word16
+  -- in read (expressionWords !! 0) :: Word16
+  in case expressionWords of
+    [word] -> read word :: Word16
+    [word1, "AND", word2] -> (lookupOrRead map word1) .&. (lookupOrRead map word2)
 
 executeLine :: (Map String Word16) -> String -> Map String Word16
 executeLine map line =
   let expressions = splitOn "->" line
       leftExpression = expressions !! 0
       rightExpression = expressions !! 1
-      num = read leftExpression :: Word16
+      num = evaluate map leftExpression
       var = trim rightExpression
   in Map.insert var num map
 
